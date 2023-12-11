66cdb7c45fa0874e838dd07e22f1884d6b44c4e8
fhoehle <fhoehle@users.noreply.github.com>
2023-12-11 11:12:21 +0100

    fix white space
    Co-authored-by: Uwe L. Korn <xhochy@users.noreply.github.com>

--- recipe/install.bat
+++ recipe/install.bat
@@ -7,6 +7,6 @@ if errorlevel 1 exit 1
 if %PKG_NAME%==libiconv (
     if exist %LIBRARY_PREFIX%\bin\iconv.exe DEL %LIBRARY_PREFIX%\bin\iconv.exe
 ) else (
-	:: relying on conda-build to deduplicate files
-	echo "Keeping all files, conda-build will deduplicate files"
+  :: relying on conda-build to deduplicate files
+  echo "Keeping all files, conda-build will deduplicate files"
 )
